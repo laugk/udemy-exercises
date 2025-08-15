6ef71faa106c9dd876d226433e7c005a2be3dc32
Guan Kiat <lauguankiat@gmail.com>
2025-08-15 10:11:30 +0800

    Initial Commit

---
+++ FirstApp/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  FirstApp
+//
+//  Created by GK on 15/8/25.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Thank you! \n");
+    return 0;
+}
