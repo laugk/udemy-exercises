d6ea9b1abe7205b72c1f27b091f72f05aa87a7c5
Guan Kiat <lauguankiat@gmail.com>
2025-08-15 10:59:45 +0800

    exercise 2

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -6,14 +6,17 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    double weight;
-    weight =20.57;
-    char ch;
-    ch = " A ";
+    int id = 5;
+    int age = 20;
+    double weight = 50.75;
+    char grade = "A";
     
-    return 0;
+    printf("The content of ID is: %d, age = %d, weight = %f\n", id, age, weight);
+           
+           return 0;
     
 }
