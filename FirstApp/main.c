15e91ed64bdd1680439404ccc47c55e325798b69
Guan Kiat <lauguankiat@gmail.com>
2025-08-16 17:41:52 +0800

    characters

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -2,22 +2,29 @@
 
 
 
-int main()
+int main(void)
 
 {
     char ch = 'A';
-    printf("Character is %c\n", ch);
-    printf("Enter a character: ");
-    char another;
-    //scanf("%c", &another);
-    another = getchar();
-    printf("You have typed : %c\n", another);
+    printf("Enter an alphabetic character: ");
+    scanf("%c", &ch);
+    
+    if (ch>='A' && ch<='Z'){
+        ch = ch + 32;
+        printf("The corresponding lower case is : %c\n",ch);
+    }
+    else if(ch>='a' && ch<='z'){
+        ch = ch - 32;
+        printf("The corresponding upper case is %c\n", ch);
+        
+    }
+        
+    else{
+        printf("The given character is not alphabetic\n");
+    }
+    
     
     
     return 200;
-    
-    
-    
-    
     
 }
