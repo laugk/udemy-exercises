eea39039f2368c60135d8cc84df719b55ae34c21
Guan Kiat <lauguankiat@gmail.com>
2025-08-16 17:53:40 +0800

    flushing problem with character input - space before %

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -5,24 +5,14 @@
 int main(void)
 
 {
-    char ch = 'A';
-    printf("Enter an alphabetic character: ");
-    scanf("%c", &ch);
-    
-    if (ch>='A' && ch<='Z'){
-        ch = ch + 32;
-        printf("The corresponding lower case is : %c\n",ch);
-    }
-    else if(ch>='a' && ch<='z'){
-        ch = ch - 32;
-        printf("The corresponding upper case is %c\n", ch);
-        
-    }
-        
-    else{
-        printf("The given character is not alphabetic\n");
-    }
-    
+    char ch;
+    int i, j;
+    printf("Enter an integer: ");
+    scanf("%d", &i);
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    printf("Enter another integer: ");
+    scanf("%d,", &j);
     
     
     return 200;
