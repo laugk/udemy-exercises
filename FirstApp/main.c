ff06c59215688b983030b333516980df4fb6c861
Guan Kiat <lauguankiat@gmail.com>
2025-08-16 18:09:26 +0800

    float and double and pi comuptation

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -5,14 +5,11 @@
 int main(void)
 
 {
-    char ch;
-    int i, j;
-    printf("Enter an integer: ");
-    scanf("%d", &i);
-    printf("Enter a character: ");
-    scanf(" %c", &ch);
-    printf("Enter another integer: ");
-    scanf("%d,", &j);
+
+    int a = 22, b = 7;
+    long double r;
+    r = (long double) a/b;
+    printf("r = %.70Lf\n", r);
     
     
     return 200;
