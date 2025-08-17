b077401713864c081896e18d6fdded1d0c7c03de
Guan Kiat <lauguankiat@gmail.com>
2025-08-17 11:07:59 +0800

    using OR function

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,21 +1,12 @@
-/**
-Instructions:
-Write a program that will input 3 integer values from user and then will
-display the average of them. You would require to declare 3 integer variables
-for holding the integers given by the users from keyboard and then you need
-another variable for holding the average. Decide by yourself about the datatype
-of the average variable.
 
-Finally print the average into console using printf.
-*/
 #include <stdio.h>
 
 int main(void)
 {
-    
+
     int i, age;
-    age = 17;
-    i = age>=13 && age<=19;
+    age = 15;
+    i = age>=10 || age<=60;
     printf("i = %d\n", i);
     
 
