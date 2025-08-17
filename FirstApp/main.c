85ff1aecb40f986e51e5056292339447c786e974
Guan Kiat <lauguankiat@gmail.com>
2025-08-17 14:04:11 +0800

    unary increment and decrement operator

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -4,11 +4,15 @@
 int main(void)
 {
 
-    int a = 100, b = 70, i;
+    int a = 5, b = 6, i;
     
-    i = !(a>b);
-    printf("i = %d\n", i);
+    i = ++a * ++b;
+    printf("a = %d, b= %d, i =%d\n", a,b, i);
     
+    
+    int c = 5;
+    printf("c = %d\n", c++); //++ is after printf, so answer is 5
+    printf("c = %d\n", ++c); //++ is before printf, and since c (from previous line is 5+1), printf = 7
 
     return 200;
 }
