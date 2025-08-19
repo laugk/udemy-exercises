7c1e8f8e658012ecedfcacec8294357b7b5477e6
Guan Kiat <lauguankiat@gmail.com>
2025-08-19 09:45:17 +0800

    AND OR operations

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -3,16 +3,15 @@
 
 int main(void)
 {
-
-    int a = 5, b = 6, i;
     
-    i = ++a * ++b;
-    printf("a = %d, b= %d, i =%d\n", a,b, i);
+    int x = -1;
     
+    int y = -2;
     
-    int c = 5;
-    printf("c = %d\n", c++); //++ is after printf, so answer is 5
-    printf("c = %d\n", ++c); //++ is before printf, and since c (from previous line is 5+1), printf = 7
-
+    int z = ++x || ++y;
+    
+    printf("%d,%d,%d", x, y, z);
+    
     return 200;
+    
 }
