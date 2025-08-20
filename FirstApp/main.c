5b9e6b58737d55ab3e88f33c9ce58b28d72e53bd
Guan Kiat <lauguankiat@gmail.com>
2025-08-20 12:14:27 +0800

    Assignment 3 IF ELSE statements

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -4,22 +4,28 @@
 int main(void)
 {
     
-    int  a, b, c, max;
+    int  a;
     
-    printf("Enter 3 numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    if(a>b && a>c)
-        max = a;
-    else if (b>c)
-        max = b;
-    else
-        max = c;
+    printf("Enter number of neighbours: \n" );
+    scanf("%d", &a);
     
-    printf("Maximum is %d\n", max);
+    if (a == 0) {
+        printf("Sad and will force you to listen to his story, will make you late.");
+    } else if (a == 1){
+        printf("He will ask you to deliver half of the pizza to his only neighbor, you may be late.\n");
+    }    else if (a >=2 && a<= 5) {
+                printf("Will have great mood and will be singing song. May crack joke as well. You will get good tips, definitely.\n");
+    }       else if (a == 7){
+                    printf("He will be making great drink for his neighbors and will invite you to taste. But will not give you tip.\n");
+    }           else if (a ==6 || a == 8){
+        printf("Too angry and furious, deliver the pizza and leave ASAP.\n");
+    }               else if (a >= 9)
+        printf("Invalid number of neighbours\n");
+        
+        
+    return 200;
+        
+    }
 
     
     
-    return 200;
-    
-    
-}
