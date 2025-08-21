ad39ac169bde91963dca1f55c4466f96d3941da6
Guan Kiat <lauguankiat@gmail.com>
2025-08-21 09:39:35 +0800

    Coding Exercise 3 : Print according to age (if else printf scanf)

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,39 +1,41 @@
-
 #include <stdio.h>
-
-int main(void)
+void print_remark(int age)
 {
-    
-    double income, tax, tax0, tax10, tax20, tax25;
-    
-    tax0 = 0.0;
-    tax10= 0.1;
-    tax20= 0.2;
-    tax25= 0.25;
-    
-    printf("Enter your income: ");
-    scanf("%lf", &income);
-    if (income < 10000) {
-        printf("Tax amount is %f\n", income * tax0 );
-    }
-        else if (income >= 10000 && income < 20000) {
-            printf("Tax amount is %f\n", income * tax10);
-        }
-        
-        else if (income >= 20000 && income < 50000) {
-            printf("Tax amount is %f\n", income * tax20);
-        }
-        else if (income > 50000) {
-            printf("Tax amount is %f\n", income * tax25 * 1.1);
-            
-        }
-        else {
-            printf("Error 404 : Non recognised character found");
-        }
-        
-    return 200;
-        
-    }
+    // --- DO NOT CHANGE ANYTHING ABOVE
+    // --- age is available here(you do not need to declare) in the variable age, auto-tester will make a call to
+    // this function and expects a printout from this function according to the logic as described.
+    // age > 0 and < 13 ----> "Child"
+    // age >=13 and < 20 -----> "Teen"
+    // age >= 20 and < 45 -----> "Young"
+    // age >= 45 and < 60 -----> "Mid-aged"
+    // age >= 60 ----> "Wise"
+    // age <= 0 ---> "Invalid"
+    // Please note that after printing remark your program must print a new line at the end.
+    // ---- Write your code below ----
 
     
+    //printf("Enter age: \n" );
+    scanf("%d", &age);
     
+    if (age <=0) {
+        printf("Invalid\n");
+    }
+    else if (age>0 && age <13){
+        printf("Child\n");
+    }
+    
+    else if (age>=13 && age<20){
+        printf("Teen\n");
+    }
+    else if (age>=20 && age<45){
+        printf("Young\n");
+    }
+    else if (age>=45 && age<60){
+        printf("Mid-aged\n");
+    }
+    else if (age >=60){
+        printf("Wise\n");
+    }
+    else printf("Invalid");
+    
+}
