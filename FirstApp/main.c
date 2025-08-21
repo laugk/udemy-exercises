54d470a7ee8d193dd459bf51f39bc861ef614b68
Guan Kiat <lauguankiat@gmail.com>
2025-08-21 10:34:22 +0800

    conditional operator (condition) ? expression_true : expression_false

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,41 +1,11 @@
 #include <stdio.h>
-void print_remark(int age)
-{
-    // --- DO NOT CHANGE ANYTHING ABOVE
-    // --- age is available here(you do not need to declare) in the variable age, auto-tester will make a call to
-    // this function and expects a printout from this function according to the logic as described.
-    // age > 0 and < 13 ----> "Child"
-    // age >=13 and < 20 -----> "Teen"
-    // age >= 20 and < 45 -----> "Young"
-    // age >= 45 and < 60 -----> "Mid-aged"
-    // age >= 60 ----> "Wise"
-    // age <= 0 ---> "Invalid"
-    // Please note that after printing remark your program must print a new line at the end.
-    // ---- Write your code below ----
 
+int main(void) {
     
-    //printf("Enter age: \n" );
+    int age;
+    printf("Enter your age: ");
     scanf("%d", &age);
+    age >=13 && age<=19 ? printf ("Yes teenage\n") : printf( "No not teenage\n");
     
-    if (age <=0) {
-        printf("Invalid\n");
-    }
-    else if (age>0 && age <13){
-        printf("Child\n");
-    }
-    
-    else if (age>=13 && age<20){
-        printf("Teen\n");
-    }
-    else if (age>=20 && age<45){
-        printf("Young\n");
-    }
-    else if (age>=45 && age<60){
-        printf("Mid-aged\n");
-    }
-    else if (age >=60){
-        printf("Wise\n");
-    }
-    else printf("Invalid");
-    
+    return 0;
 }
