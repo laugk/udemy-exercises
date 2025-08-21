48486f8a8276c495bcfd92ea0b5dc5b76e7f8eb9
Guan Kiat <lauguankiat@gmail.com>
2025-08-21 13:16:55 +0800

    switch usage

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -4,24 +4,18 @@
 
 int main(void){
     
-    int year;
-    printf("Enter year: ");
-    scanf("%d", &year);
-//    if (year % 4 == 0)
-//      if (year % 100 == 0 )
-//        if (year % 400 == 0)
-//          printf ("Yes the year is a leap year\n");
-//       else
-//          printf("No this is not a leap year\n");
-//      else
-//         printf("Yes this is a leap year\n");
-//    else
-//      printf("No this is not a leap year\n");
- 
-        if((year % 4 == 0 && year % 100 !=0) || year % 400 == 0)
-            printf(" Yes %d is a leap year \n", year);
-    else
-        printf(" No %d is not a leap year \n", year);
-            
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n );
+    
+    switch(n) {
+        case 1: printf("One\n");
+            break;
+        case 2: printf("Two\n");
+            break;
+        case 3: printf("Three\n");
+            break;
+        default: printf("Invalid number\n");
+    }
     return 0;
 }
