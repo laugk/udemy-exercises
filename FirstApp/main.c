5fd3d5161419c877d5bed2ff54ec9943a9bd7e35
Guan Kiat <lauguankiat@gmail.com>
2025-08-21 11:42:53 +0800

    Leap Year calculator, Nested if-else

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
+//leap year calculation
+
+int main(void){
     
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    age >=13 && age<=19 ? printf ("Yes teenage\n") : printf( "No not teenage\n");
-    
+    int year;
+    printf("Enter year: ");
+    scanf("%d", &year);
+//    if (year % 4 == 0)
+//      if (year % 100 == 0 )
+//        if (year % 400 == 0)
+//          printf ("Yes the year is a leap year\n");
+//       else
+//          printf("No this is not a leap year\n");
+//      else
+//         printf("Yes this is a leap year\n");
+//    else
+//      printf("No this is not a leap year\n");
+ 
+        if((year % 4 == 0 && year % 100 !=0) || year % 400 == 0)
+            printf(" Yes %d is a leap year \n", year);
+    else
+        printf(" No %d is not a leap year \n", year);
+            
     return 0;
 }
