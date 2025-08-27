f19a000dce1e6c39fc7296dd62d40d036dfc793d
Guan Kiat <lauguankiat@gmail.com>
2025-08-27 13:10:22 +0800

    Assignment 6 : Pattern printing Question 2 double for loop

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -4,13 +4,23 @@
 int main(void) {
     
     
-    int n;      ///line numbers
+    int n1, n2;      ///line numbers
     int i, j;   /// i = line number, j = column number in the line
     
     printf("Enter how many lines: ");
-    scanf("%d", &n);
+    scanf("%d", &n1);
+    n2 = n1;
     
-    for (i = n; i >= 1; --i) {
+    for(i = 1; i <= n1; ++i ) {
+        for (j = 1; j<=i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    
+    
+    for (i = n2; i >= 1; --i) {
         for (j = 1; j <= i; ++j) {
             printf("*");
         }
