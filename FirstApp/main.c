e4ac6b6302cccf18a5ab52c7fb23546c9963de00
Guan Kiat <lauguankiat@gmail.com>
2025-08-28 12:31:02 +0800

    commit blank

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void) {
+    int guess;
+
+srand(time(NULL));
+
+    int d1 = rand() % 6 + 1;
+    int d2 = rand() % 6 + 1;
+    
+    printf("Computer rolled two dice...Guess the sum\nEnter any <=0 value to terminate\n");
+    scanf("%d", &guess);
+    
+    while (guess >0) {
+        if (guess > d1 + d2) {
+            printf("Guess is high");
+        }
+        else if (guess < d1 + d2) {
+            printf("Guess is low");
+        }
+        else if (guess == d1 + d2) {
+            printf("Guess is correct!");
+        }
+    }
+
+return 0;
+
+}
 
