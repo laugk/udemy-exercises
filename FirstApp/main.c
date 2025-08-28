92f69dc5dc97a49df66e4590756b23b4b000e208
Guan Kiat <lauguankiat@gmail.com>
2025-08-28 09:39:39 +0800

    Assignment 6 : Pattern Printing Q4 Print with number

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -3,25 +3,21 @@
 
 int main(void) {
     
-    
-    int n1, n2;      ///line numbers
-    int i, j;   /// i = line number, j = column number in the line
-    
-    printf("Enter how many lines: ");
-    scanf("%d", &n1);
-    n2 = n1;
-    
-    for(i = 1; i <= n1; ++i ) {
-        for (j = 1; j<=i; ++j) {
-            if ((i+j)% 2 == 0) {
-                printf("0 ");
-            } else {
-                printf("1 ");
+
+        
+        int n1, n2;      ///line numbers
+        int i, j;   /// i = line number, j = column number in the line
+        
+        printf("Enter how many lines: ");
+        scanf("%d", &n1);
+        n2 = n1;
+        
+        for(i = 1; i <= n1; ++i ) {
+            for (j = 1; j<=i; ++j) {
+                printf("%d ", j);
             }
-            
+            printf("\n");
         }
-        printf("\n");
-    }
     
     
     
