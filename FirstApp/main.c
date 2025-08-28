99cb0806cb50e4be5e47867cc6b6af55da7ec935
Guan Kiat <lauguankiat@gmail.com>
2025-08-28 14:53:35 +0800

    One Dimensional Array - Storing and retriving of array values

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -4,69 +4,20 @@
 
 
 
-int main(void){
+int main(void) {
     
+    int x[100], i;
     
-    int correct = 0;
-    int incorrect = 0;
-    
-    
-    
-    int score = 0;
-    int d1, d2, guess;
-    int dicesum;
-    //    int seed;
-    //   srand(seed);
-    
-    
-    
-    
-    while (1) {
-        //d1 = 2;
-        //d2 = 3;
-                d1 = rand() % 6 + 1; // generate random number between 1 and 6 for first die
-                d2 = rand() % 6 + 1; // generate random number between 1 and 6 for second die
-        dicesum = d1 + d2;
-        
-        
-        printf("Computer rolled two dice...Guess the sum\n");
-        printf("Enter any <=0 value to terminate\n");
-        scanf("%d", &guess);
-        
-        
-        
-        if (guess<= 0) {
-            break;
-        }
-        
-        //score
-       //score += (guess == dicesum) ? correct : -incorrect;
-        else if (guess == dicesum) {
-            correct++;
-            printf("Bravo! You are correct! +10\n");
-        }
-        
-        else if (guess !=dicesum) {
-            incorrect++;
-            printf("Darn! Try again! -5\n");
-        }
-        
-        printf("Dice roll was %d\n\n", dicesum);
-        
-        
-        printf("Total Correct: %d\n", correct);
-        printf("Total Incorrect: %d\n", incorrect);
-        score = (correct * 10 + incorrect *-5);
-        printf("Your score is %d\n\n", score);
+    for (i=0; i<100; i++) {
+        x[i] = i+1;
     }
     
-    printf("Your final score is %d\n", score);
-    
-    return 0;
-    
+    printf("Content of the array: ");
+    for(i=0; i<100; i++ ) {
+        printf("%d, ", x[i]);
+    }
 }
-        
-        
+
     
     
     
