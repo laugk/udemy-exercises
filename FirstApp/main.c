32417adc247d8d1988e5f7936c397baeac28527f
Guan Kiat <lauguankiat@gmail.com>
2025-08-28 10:04:33 +0800

    Continue Function - print odd numbers

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -9,13 +9,12 @@ int main(void) {
 
     
     for(count=1;count<=100;count++) {
+
+        if (count%2 == 0)
+            continue;
+        
         printf("Count = %d\n", count);
-        printf("print next? enter Y/y to continue or any other key to terminate ");
-        scanf(" %c", &response);
-        if (response != 'Y' && response != 'y')
-            break;
     }
-    printf("Terminated at count = %d\n", count);
     
     return 0;
     
