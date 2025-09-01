1025131bebbd6ad22f8529674fb52cecb74eae20
Guan Kiat <lauguankiat@gmail.com>
2025-09-01 17:55:50 +0800

    2D array programming example - Sale Person storage and retrieval of information

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,53 +1,44 @@
 #include <stdio.h>
 
 int main(void) {
-    int arr[10] = {0};  // Declare and initialize the array with size 10
-    int n, i, d;
-
-    printf("You can input at most 10 digits number\n");
-    printf("Enter total number of digits in the number: ");
-    scanf("%d", &n);
-
-    // Check if the number of digits is valid
-    if (n > 10) {
-        printf("Error: Number of digits exceeded limit.\n");
-        return 1;  // Return error code
+    int r, c;
+    // r is no of salesperson, c is no of items that they sell
+    //input for r and c
+    
+    printf("Enter number of salesperson: ");
+    scanf("%d", &r);
+    printf("Enter number of items: ");
+    scanf("%d", &c);
+    printf("\n\n\n");
+    
+    //declare 2d array
+    double saleData [r][c];
+    //input saledata into the array
+    printf("Enter sale data: \n\n");
+    int i,j;
+    for (i=0;i<r;i++) {
+        for (j=0;j<c; j++){
+            printf("Sale amt for salesperson no %d item no %d: ", i+1, j+1);
+            scanf("%lf", &saleData [i][j]);
+        }
     }
-
-    // Input each digit one at a time and store it in the array
-    for (i = 10 - n; i < 10; i++) {
-        printf("Enter digit #%d: ", 10 - i);
-        scanf("%d", &arr[i]);
-    }
-
-    printf("\nThe input number is: ");
-    for (i = 0; i < 10; i++) {
-        printf("%d", arr[i]);
-    }
-
-    // Input the digit to be added
-    printf("\nEnter a digit between 1 and 9 to add with the number: ");
-    scanf("%d", &d);
-
-    // Check if the digit is valid
-    if (d < 1 || d > 9) {
-        printf("Error: Invalid digit.\n");
-        return 2;  // Return error code
-    }
-
-    // Add the digit to the number starting from index 9
-    int carry = d;
-    for (i = 9; i >= 0 && carry > 0; i--) {
-        int sum = arr[i] + carry;
-        arr[i] = sum % 10;
-        carry = sum / 10;
-    }
-
-    printf("\nThe number after addition is: ");
-    for (i = 0; i < 10; i++) {
-        printf("%d", arr[i]);
+    //display report
+    printf("----------------------------\n");
+    printf("Sale Data\n");
+    printf("----------------------------\n\n");
+    double total = 0.0;
+    for (i=0; i<r;i++) {
+        printf("Sale data for salesperson no. %d\n",i+1);
+        printf("----------------------------\n\n");
+        total = 0.0;
+        for (j=0;j<c;j++) {
+            printf("\tItem %d:   $ %.2f\n", j+1, saleData [i][j]);
+            total += saleData [i][j];
+        }
+        printf("\tTotal:   $ %.2f\n", total);
+        printf("\n\n");
         
     }
-    printf("\n");
+    
     return 0;
 }
