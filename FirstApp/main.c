e8296b38ea07cd07c915c8ab40d47bb417e4bd69
Guan Kiat <lauguankiat@gmail.com>
2025-09-02 11:15:39 +0800

    Assignment 11: Print Transpose of a matrix

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,44 +1,78 @@
 #include <stdio.h>
 
 int main(void) {
-    int r, c;
-    // r is no of salesperson, c is no of items that they sell
-    //input for r and c
+    int x, y;
+    //x is row y is column
+    //input for x and y
     
-    printf("Enter number of salesperson: ");
-    scanf("%d", &r);
-    printf("Enter number of items: ");
-    scanf("%d", &c);
-    printf("\n\n\n");
+    printf("Enter number of rows for the matrix (>3 and <=10: ");
+    scanf("%d", &x);
     
-    //declare 2d array
-    double saleData [r][c];
-    //input saledata into the array
-    printf("Enter sale data: \n\n");
-    int i,j;
-    for (i=0;i<r;i++) {
-        for (j=0;j<c; j++){
-            printf("Sale amt for salesperson no %d item no %d: ", i+1, j+1);
-            scanf("%lf", &saleData [i][j]);
-        }
-    }
-    //display report
-    printf("----------------------------\n");
-    printf("Sale Data\n");
-    printf("----------------------------\n\n");
-    double total = 0.0;
-    for (i=0; i<r;i++) {
-        printf("Sale data for salesperson no. %d\n",i+1);
-        printf("----------------------------\n\n");
-        total = 0.0;
-        for (j=0;j<c;j++) {
-            printf("\tItem %d:   $ %.2f\n", j+1, saleData [i][j]);
-            total += saleData [i][j];
-        }
-        printf("\tTotal:   $ %.2f\n", total);
-        printf("\n\n");
+    if ( x>3 && x <=10) {
         
+        printf("Enter number of columns for the matrix (>3 and <=10: ");
+        scanf("%d", &y);
+        
+        if ( y > 3 && y <=10) {
+            
+            
+            //storing of data into 2d array
+            int i,j;
+            int arr1 [x][y];
+            
+            for (i=0; i<x; i++) {
+                for(j=0;  j<y; j++) {
+                    printf("Matrix [%d] [%d]: ", i, j);
+                    scanf("%d",&arr1 [i][j]);
+                }
+            }
+            
+            /* recalling data from 2d array
+             for (i=0; i<x; i++) {
+             for(j=0;  j<y; j++) {
+             printf("Matrix [%d] [%d]: %d\n", i, j, arr1[i][j]);
+             }
+             }
+             */
+            
+            //transpose
+
+            int arr2[y][x];
+            
+            for (i=0; i<y; i++) {
+                for (j=0; j<x; j++) {
+                    arr2[i][j]= arr1[j][i];
+                }
+            }
+            
+            //print transposed matrix
+            
+            printf("Matrix as given by you: \n");
+            for (i=0; i<x; i++) {
+                for(j=0;  j<y; j++) {
+                    printf("%4d", arr1[i][j]);
+                }
+                printf("\n");
+            }
+            
+            printf("Transpose of the given matrix: \n");
+            for (i=0; i<y; i++) {
+                for (j=0; j<x; j++) {
+                    printf("%4d", arr2[i][j]);
+                }
+                printf("\n");
+            }
+            
+        } else {
+            printf("Invalid input for the number of columns\n");
+            }
+        } else {
+            printf("Invalid input for the number of rows.\n");
+        }
+        
+    return 0;
     }
     
-    return 0;
-}
+  
+   
+
