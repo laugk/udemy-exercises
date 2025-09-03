8f4b865abf22bdcf9c1bc9e3d4c44f94076e78bb
Guan Kiat <lauguankiat@gmail.com>
2025-09-03 14:33:55 +0800

    Using strcat to combine text strings

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -3,11 +3,16 @@
 
 int main(void){
 
-    char first[80] = "Chocolate Candy";
-    char second[80];
+    char first[80] = "Chocolate";
+    char second[80] = "Candy";
     
-    strcpy(second, first); //copy string from first to second
-    printf("Copied string %s\n", second);
+    //strcpy(second, first); //copy string from first to second
+    //printf("Copied string %s\n", second);
+    
+    strcat (first, " ");
+    strcat (first, second);
+    
+    printf("The first array contains %s\n", first);
     
     
     return 0;
