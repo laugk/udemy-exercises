5e560f4893cc0a0fde752cfe6ec1d17e2c2d4b27
Guan Kiat <lauguankiat@gmail.com>
2025-09-03 14:02:30 +0800

    Counting length of string with for loop

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -3,10 +3,13 @@
 int main(void){
 
     char str[80];
-    printf("Enter your name: ");
-    scanf("%s", str);
-    printf("Welcome, %s\n", str);
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    int i;
     
+    for (i=0; str[i]!='\0'; i++);
+    printf("The string length is %d\n", i);
     
     
     
