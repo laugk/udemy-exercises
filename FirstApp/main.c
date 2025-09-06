f22b9fd834dbf25629fa044cdce9c10875325556
Guan Kiat <lauguankiat@gmail.com>
2025-09-06 21:45:28 +0800

    Find Prime number and Sum of Prime with Prime Function

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -11,14 +11,33 @@ int isPrime(int n) {
     return 1;
 }
 
-
-int main(void) {
-    int count;
-    for(count=10; count<=100; count++)
+int sumOfPrimes (int lb, int ub) {
+    int count, sum;
+    
+    for (count=lb, sum = 0; count <=ub; count++) {
         if (isPrime(count))
-            printf("%d is Prime\n", count);
-    
-    
-    return 0;
+            sum +=count;
+    }
+    return sum;
 }
 
+
+
+
+
+int main(void) {
+    /*    int count;
+     for(count=10; count<=100; count++)
+     if (isPrime(count))
+     printf("%d is Prime\n", count);
+     
+     
+     return 0;
+     }
+     */
+    
+    
+    int s;
+    s = sumOfPrimes(10,200);
+    printf("Sum of primes in the range %d-%d is %d\n", 10, 200, s);
+}
