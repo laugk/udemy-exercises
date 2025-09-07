8b05b672f65dd2d857f1efc464f9044cb61d1fdb
Guan Kiat <lauguankiat@gmail.com>
2025-09-07 20:47:39 +0800

    Basic Recursion Example

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,120 +1,19 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include <stdio.h> // Preprocessor directive to include the standard input/output library, which contains functions like printf and scanf
 
-#define rock 1  //telling the complier that rock is 1
-#define paper 2
-#define scissor 3
-#define Player1_Win 1
-#define Player2_Win 2
-#define DRAW 0
+// Function declaration for printing a message recursively
+void printMsg(unsigned int n) { // Define a function called printMsg that takes an unsigned integer parameter n
+    if (n == 0) // Check if the value of n is equal to 0
+        return; // If it is, end the current function call and return to the previous one (base case for recursion)
 
-    int rock_paper_scissor(int player1, int player2) {
-        int result;
-        if (player1 == rock && player2 == paper) {
-            result = Player2_Win;
-        } else if (player1 == rock && player2 == scissor) {
-            result = Player1_Win;
-        } else if (player1 == rock && player2 == rock) {
-            result = DRAW;
-        } else if (player1 == paper && player2 == paper) {
-            result = DRAW;
-        } else if (player1 == paper && player2 == scissor) {
-            result = Player2_Win;
-        } else if (player1 == paper && player2 == rock) {
-            result = Player1_Win;
-        } else if (player1 == scissor && player2 == paper) {
-            result = Player1_Win;
-        } else if (player1 == scissor && player2 == scissor) {
-            result = DRAW;
-        } else if (player1 == scissor && player2 == rock) {
-            result = Player2_Win;
-        }
-        return result;
-    }
+    printf("Hello World\n"); // Print "Hello World" followed by a newline character to the console
 
-int get_user_input (void) {
-    int choice;
-    do {
-        printf("1. Rock\n");
-        printf("2. Paper\n");
-        printf("3. Scissor\n");
-        printf("Please input [1,2,3]: ");
-        scanf("%d", &choice);
-        if (! (choice >=1 && choice <=3)) {
-            printf("Error -- Invalid input");
-        }
-    } while (!(choice >=1 && choice <=3));
-    return choice;
-               
+    n--; // Decrement the value of n by 1
 
-    
-}
-void print_word(int v) {
-    if (v == rock) {
-        printf("Rock\n");
-    } else if (v == paper) {
-        printf("Paper\n");
-    } else if (v == scissor) {
-        printf("Scissor\n");
-    }
+    printMsg(n); // Recursive call: Call the printMsg function with the updated value of n (recursive case)
 }
 
-
-int main(void) {
-    int user_score = 0;
-    int computer_score = 0;
-    srand(time(NULL));
-    
-    int quit = 0;
-    while (!quit) {
-        
-        int user_input = get_user_input();
-        int computer_choice = rand() % 3 + 1; //plus 1 because the default is 0,1,2 and our decision is 1,2,3
-        printf("Computer chose: ");
-        print_word(computer_choice);
-        
-        printf("User chose: ");
-        print_word(user_input);
-        
-        int result = rock_paper_scissor(user_input, computer_choice);
-        if (result == DRAW) {
-            printf("It is a draw\n");
-        } else if (result == Player1_Win) {
-            printf("User wins");
-            user_score++;
-        } else if (result == Player2_Win) {
-            printf("Computer wins");
-            computer_score++;
-        }
-        printf("\n----------------------------\n");
-        printf("User Score: %d\n", user_score);
-        printf("Computer Score: %d\n", computer_score);
-        printf("----------------------------\n");
-        printf("\n\n");
-        
-        
-        printf(" If you want to continue input any integer, 0 to terminate. ");
-        int response;
-        scanf("%d", &response);
-        if (response == 0) {
-            quit = 1;
-        }
-    } //end of while
-    
-    printf("Final Score...\n");
-    printf("\n");
-    printf("----------------------------\n");
-    printf("User Score: %d\n", user_score);
-    printf("Computer Score: %d\n", computer_score);
-    printf("----------------------------\n");
-    printf("\n\n");
-    if (user_score == computer_score) {
-        printf("Game Draw\n");
-    } else if (user_score > computer_score) {
-        printf("User Wins!\n");
-    } else if (user_score < computer_score) {
-        printf("Computer Wins");
-    }
-    
+// Main function where program execution begins
+int main(void) { // Define the main function, which returns an integer and takes no arguments
+    printMsg(3); // Call the printMsg function with an argument of 3
+    return 0; // Return 0 to indicate that the program has executed successfully
 }
