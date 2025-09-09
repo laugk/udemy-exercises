22c68de8ec2e8671de48d473f4112cec9efcc7a6
Guan Kiat <lauguankiat@gmail.com>
2025-09-09 14:22:51 +0800

    Coding Exercise 11: Passing one dimensional array as parameter

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,32 +1,55 @@
-#include <stdio.h>
+#include <iostream>
 
+using namespace std;
 
-// Function definition for 'toggleChar', which takes a pointer to a character as an argument.
-void toggleChar(char *ptr) {
-    int i; // Declare a local integer variable 'i' to be used in the loop counter.
+// declare the prototype of get_max
+int get_max(int *arr, int n);
 
-    // Start of a for-loop that iterates until it reaches the null terminator ('\0') at the end of the string.
-    for (i = 0; *(ptr + i) != '\0'; i++) {
-        // Check if the current character pointed to by 'ptr' is an uppercase letter.
-        if (*(ptr + i) >= 'A' && *(ptr + i) <= 'Z')
-            // If it is, add 32 to its ASCII value to convert it into a lowercase letter.
-            *(ptr + i) = *(ptr + i) + 32;
-        // Check if the current character pointed to by 'ptr' is a lowercase letter.
-        else if (*(ptr + i) >= 'a' && *(ptr + i) <= 'z')
-            // If it is, subtract 32 from its ASCII value to convert it into an uppercase letter.
-            *(ptr + i) = *(ptr + i) - 32;
+// declare the prototype of get_min
+
+int get_min(int *arr, int n);
+
+// Now implement the function get_max
+
+int get_max(int *arr, int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
     }
+    return max;
 }
 
-// Main function definition for the program.
-int main(void) {
-    char str[100]; // Declare a character array 'str' with a size of 100 to store user input string.
+// Now implement the function get_min
 
-    printf("Enter a string: "); // Prompt the user to enter a string.
-    scanf("%[^\n]", str); // Read user input and store it in the 'str' array until a newline character ('\n') is encountered.
 
-    toggleChar(str); // Call the 'toggleChar' function, passing the address of the first character in the 'str' array as an argument.
-    printf("Toggled characters: %s\n", str); // Print the modified string after toggling the case of each character.
+int get_min(int *arr, int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+    
+}
 
-    return 0; // Return 0 to indicate successful execution of the program.
+
+void get_max_min_sum(int *arr, int n, int *p_sum){
+    
+    // call get_max to get the maximum in the array arr, you will need to pass the base address arr and n to this function.
+    
+    int max = get_max(arr,n);
+    
+    
+    
+    // Call get_min in the similar way and get the minimum value in the array, here also you will need to pass 2 parameters, base address of the array and the number of elements in that array.
+    
+    int min = get_min(arr,n);
+    
+    // find the sum of the maximum and minimum and then assign the sum to the integer pointed by p_sum.
+    
+    *p_sum = max + min;
+    
 }
