4d57e6fabea36df0d4d7975e6c20af68ceb1ec52
Guan Kiat <lauguankiat@gmail.com>
2025-09-09 08:19:00 +0800

    Coding Exercise 10: update values from function at the original location

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
+#include <iostream>
 
-int main (void) { // Function definition for the main function, which is the entry point of a C program
-    int *p, *q; // Declare two pointer variables 'p' and 'q' that can store addresses of integer values
-    int **k; // Declare a double pointer variable 'k' that can store the address of another pointer variable
-    int x = 100; // Declare and initialize an integer variable 'x' with the value 100
-    p = &x; // Assign the address of 'x' to the pointer variable 'p', making 'p' point to 'x'
+using namespace std;
+
+
+/**
+ * The following function update receives 2 parameters, both of them are integer pointers as you can see.
+ * They will receive address of 2 integer variables by the tester, those integer variables will be declared by the
+ * tester.
+ * Your task will be to update those integer variables declared by tester using these pointers p and q.
+ */
+int update(int *p, int *q){
+    // Instruction 1 : Update the integer variable pointed by p: add 10 with that.
     
-    k = &p; // Assign the address of 'p' to the double pointer variable 'k', making 'k' point to 'p'
-    printf("%d\n", **k); // Print the value of 'x' indirectly by dereferencing the double pointer 'k' twice (once for 'p' and once for 'x')
+    *p += 10;
     
-    **k = **k + 1; // Increment the value of 'x' by 1 indirectly using the double pointer 'k', which points to 'p' that points to 'x'
-    printf("%d\n", x); // Print the updated value of 'x' directly to verify the increment operation
-    return 0; // Return 0 to indicate successful execution of the program
+    // Instruction 2: Update the integer variable pointed by q: subtract 5 from that.
+    
+    *q -=5;
+    // Instruction 3: You should return the product of those 2 values from this function using return after the
+    // above updations.
+    
+    
+    return *p * *q;
     
 }
