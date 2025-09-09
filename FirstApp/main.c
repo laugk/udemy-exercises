eaa7c67d719a714baac7878470f77d6bb0c7ea7c
Guan Kiat <lauguankiat@gmail.com>
2025-09-09 08:41:38 +0800

    Combining pointers and function

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,26 +1,20 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
 
 
-/**
- * The following function update receives 2 parameters, both of them are integer pointers as you can see.
- * They will receive address of 2 integer variables by the tester, those integer variables will be declared by the
- * tester.
- * Your task will be to update those integer variables declared by tester using these pointers p and q.
- */
-int update(int *p, int *q){
-    // Instruction 1 : Update the integer variable pointed by p: add 10 with that.
-    
-    *p += 10;
-    
-    // Instruction 2: Update the integer variable pointed by q: subtract 5 from that.
-    
-    *q -=5;
-    // Instruction 3: You should return the product of those 2 values from this function using return after the
-    // above updations.
-    
-    
-    return *p * *q;
+void doWork(int a, int b, int c, int *p, int *q) {
+    *p = a + b + c;
+    if (a>b && a >c)
+        *q = a;
+    else if (b>c)
+        *q = b;
+    else
+        *q = c;
+}
+
+
+int main(void) {
+    int x = 10, y = 50, z = 15, sum, max;
+    doWork(x,y,z, &sum, &max);
+    printf("Sum of %d, Max = %d\n", sum, max);
     
 }
