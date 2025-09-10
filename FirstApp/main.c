13897ac60d2a9a90b8c97142e202f71bdb1ad485
Guan Kiat <lauguankiat@gmail.com>
2025-09-10 15:52:48 +0800

    Dynamic Memory Allocation - Malloc function

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+//malloc, calloc, realloc
 
 int main (void) {
-    void *vp;
-    int x = 100;
-    vp = &x;
     
-    printf("%d\n", *((int *)vp));
+    int *p;
+    int n;
+    printf("Enter how many integers: ");
+    scanf("%d", &n);
     
+    p = (int *)malloc(n * sizeof(int));
+    if (p==NULL) {
+        printf("Unable to allocate memory\nExiting the program\n");
+        exit(1);
+    }
+    //use the allocated space pointed by p for keeping values
+    
+    int i;
+    for (i=0; i<n; i++) {
+        printf("Next number: ");
+        scanf("%d", p+i);
+    }
+    
+    printf("Content of the array: ");
+    for (i=0; i<n; i++) {
+        printf("%4d",*(p+i));
+    }
+    printf("\n");
     
     
     return 0;
